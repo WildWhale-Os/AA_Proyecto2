1c97f9acd78179889ac1ab37ee8a43716e22a57d
Tomas Bravo <tomas.b.c@outlook.com>
2022-05-24 19:37:39 -0400

    First commit

---
+++ CMakeFiles/proyecto2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto2.dir/src/main.cpp.o"
+  "CMakeFiles/proyecto2.dir/src/main.cpp.o.d"
+  "proyecto2"
+  "proyecto2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
